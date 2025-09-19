d922d4cb840165da0bb1850df7795f753c12e2f5
brunovigo24 <brunohvigo@gmail.com>
2025-09-18 22:22:38 -0300

    feat: adicionar entidade Task com propriedades básicas e gerenciamento de timestamps

---
+++ backend/src/tasks/entities/task.entity.ts
@@ -0,0 +1,22 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+
+@Entity('tasks')
+export class Task {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ nullable: false })
+  title: string;
+
+  @Column({ nullable: true })
+  description: string;
+
+  @Column({ default: false })
+  completed: boolean;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
